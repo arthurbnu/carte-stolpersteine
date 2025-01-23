b0eb1c22fe40326d1e17c2a06e61a93baaec72e9
arthurbnu <arthur.brody@bnu.fr>
2025-01-23 17:59:18 +0100

    remplissage premières villes

--- components/StolDetail.vue
+++ components/StolDetail.vue
@@ -2,7 +2,7 @@
     <section 
         class = "absolute z-[1000] right-0 top-0 w-[25vw] h-full !mt-0 p-4 bg-white/90 transition-all duration-500 ease-in-out will-change-[right]"
         :class = "{'right-[-25vw]' : !mounted}">
-        <h2>{{ stolperstein.stolpersteinLabel.value }}</h2>
+        <h2 class="text-lg">{{ stolperstein.stolpersteinLabel.value }}</h2>
         <p>
             <a :href="stolperstein.stolperstein.value" target="_blank" class = "text-blue-500 underline">
                 Voir sur Wikidata</a>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
 
     <main>
+        <!-- <input type="text" v-model="currentCity" id = "current-city"/> -->
+         <select v-model="currentCity" id = "current-city">
+            <option v-for="city in cities" :value="city.id">{{ city.name }}</option>
+        </select>
         <section class="w-[100vw] h-[100vh]">
             <LMap v-if="!pending" ref="map" :zoom="zoom" @click="clickedMarker = null"
                 :center="[sparqlResult[0].latitude.value, sparqlResult[0].longitude.value]"
@@ -40,19 +44,45 @@ const clickedMarker = ref(null);
 const zoom = ref(15);
 const center = ref([47.413220, -1.219482]);
 
-const request = `
-#defaultView:Map
-SELECT ?stolperstein ?stolpersteinLabel ?coords ?latitude ?longitude WHERE {
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],fr". }
-  ?stolperstein (wdt:P31/wdt:P279*) wd:Q26703203.
-  ?stolperstein wdt:P131 wd:Q22741.
-  ?stolperstein wdt:P625 ?coords.
-  BIND(geof:latitude(?coords) AS ?latitude)
-  BIND(geof:longitude(?coords) AS ?longitude)
-}`
+const cities = [
+    {name: 'Saverne', id: 'Q22741'},
+    {name: 'Strasbourg', id : 'Q6602'},
+    {name: 'Bouxwiller ', id: 'Q22740'},
+    // {name: 'Benfeld', id: 'Q22776'},
+    // {name: 'Balbrom', id: 'Q22793'},
+    // {name: 'Barr ', id: 'Q22758'},
+    // {name: 'Bischwiller ', id: 'Q22431'},
+    // {name: 'Gries ', id: 'Q22481'},
+]
 
-const url = `https://query.wikidata.org/sparql?query=${encodeURIComponent(request)}&format=json`
+const currentCity = ref(cities[0].id);
+
+
+const request = computed( () => 
+`#defaultView:Map
+    SELECT ?stolperstein ?stolpersteinLabel ?coords ?latitude ?longitude WHERE {
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],fr". }
+      ?stolperstein (wdt:P31/wdt:P279*) wd:Q26703203.
+      ?stolperstein wdt:P131 wd:${currentCity.value}.
+      ?stolperstein wdt:P625 ?coords.
+      BIND(geof:latitude(?coords) AS ?latitude)
+      BIND(geof:longitude(?coords) AS ?longitude)
+    }`
+)
+
+const url = computed(() => `https://query.wikidata.org/sparql?query=${encodeURIComponent(request.value)}&format=json`)
 const headers = { 'Accept': 'application/json' }
 const { data: sparqlResult, error, pending, execute: refresh } = await useFetch(url, { headers: headers, server: false, transform: res => res.results.bindings });
 
 </script>
+
+<style scoped>
+
+#current-city{
+    position: fixed;
+    top: 20px;
+    left: 48vw;
+    z-index: 1000;
+}
+
+</style>
